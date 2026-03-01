cd7d0eca22f4270c72cb0e892ff51b81e0439081
Kiryl Makhveyenia <mahveenya.k.a@gmail.com>
2026-03-01 20:30:00 +0200

    refactor: remove duplicated imports

--- backend/app/db.py
+++ backend/app/db.py
@@ -1,17 +1,16 @@
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
-from sqlalchemy.orm import DeclarativeBase
 import os
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.ext.asyncio import (
+    create_async_engine,
+    async_sessionmaker,
+    AsyncSession,
+)
 
 DATABASE_URL = os.getenv(
     "DATABASE_URL",
     "postgresql+asyncpg://admin:admin@db:5432/pokemon_db",
 )
 
-from sqlalchemy.ext.asyncio import (
-    create_async_engine,
-    async_sessionmaker,
-    AsyncSession,
-)
 
 engine = create_async_engine(DATABASE_URL)
 
