a2e91e0a5183f067814a5777c4e87ef4ecc0895c
Kiryl Makhveyenia <mahveenya.k.a@gmail.com>
2026-04-20 22:52:35 +0300

    fix: use ability base url for ability url

--- app/routers/pokemon_router.py
+++ app/routers/pokemon_router.py
@@ -4,7 +4,6 @@ from app.constants import Endpoints
 from app.db.session import get_session
 from app.schemas.pokemon_schema import PokemonListSchema, PokemonSchema
 from app.services.pokemon_service import get_pokemon_by_id, get_pokemon_list
-from app.utils.helpers import build_base_url
 
 router = APIRouter(prefix=f"{Endpoints.POKEMON_BASE}", tags=["pokemon"])
 
@@ -16,8 +15,7 @@ async def list_pokemons(
     limit: int = 20,
     session=Depends(get_session),
 ) -> PokemonListSchema:
-    base_url = f"{build_base_url(request)}{Endpoints.POKEMON_BASE}"
-    return await get_pokemon_list(session, offset, limit, base_url)
+    return await get_pokemon_list(session, offset, limit, request)
 
 
 @router.get(
@@ -32,8 +30,7 @@ async def get_pokemon(
     request: Request, id_or_name: str, session=Depends(get_session)
 ) -> PokemonSchema:
     if id_or_name.isdigit():
-        base_url = f"{build_base_url(request)}{Endpoints.POKEMON_BASE}"
-        pokemon = await get_pokemon_by_id(session, int(id_or_name), base_url)
+        pokemon = await get_pokemon_by_id(session, int(id_or_name), request)
         if pokemon is None:
             raise HTTPException(status_code=404, detail="Pokemon not found")
         return pokemon
--- app/services/pokemon_service.py
+++ app/services/pokemon_service.py
@@ -1,3 +1,4 @@
+from app.constants import Endpoints
 from app.repositories.ability_repository import get_ability_db_models_by_pokemon_id
 from app.repositories.pokemon_repository import (
     count_pokemon_db_models,
@@ -10,19 +11,18 @@ from app.schemas.pokemon_schema import (
     PokemonListSchema,
     PokemonSchema,
 )
-from app.utils.helpers import build_pagination
+from app.utils.helpers import build_base_url, build_pagination
 
 
-async def get_pokemon_list(session, offset, limit, base_url) -> PokemonListSchema:
+async def get_pokemon_list(session, offset, limit, request) -> PokemonListSchema:
     pokemons = await get_pokemon_db_models(session, offset, limit)
     if not pokemons:
         return PokemonListSchema(count=0, next=None, previous=None, results=[])
 
+    pokemon_base_url = f"{build_base_url(request)}{Endpoints.POKEMON_BASE}"
     total = await count_pokemon_db_models(session)
-
-    results = [NamedAPIResourceSchema.from_model(p, base_url) for p in pokemons]
-
-    next_url, previous_url = build_pagination(base_url, total, offset, limit)
+    results = [NamedAPIResourceSchema.from_model(p, pokemon_base_url) for p in pokemons]
+    next_url, previous_url = build_pagination(pokemon_base_url, total, offset, limit)
 
     return PokemonListSchema(
         count=total,
@@ -32,16 +32,18 @@ async def get_pokemon_list(session, offset, limit, base_url) -> PokemonListSchem
     )
 
 
-async def get_pokemon_by_id(session, pokemon_id, base_url) -> PokemonSchema | None:
+async def get_pokemon_by_id(session, pokemon_id, request) -> PokemonSchema | None:
     pokemon = await get_pokemon_db_model_by_id(session, pokemon_id)
     if not pokemon:
         return None
 
+    ability_base_url = f"{build_base_url(request)}{Endpoints.ABILITY_BASE}"
     abilities = await get_ability_db_models_by_pokemon_id(session, pokemon_id)
     abilities_resources = [
-        NamedAPIResourceSchema.from_model(a, base_url) for a in abilities
+        NamedAPIResourceSchema.from_model(a, ability_base_url) for a in abilities
     ]
     abilities_info = AbilityInfoSchema.from_resources(abilities_resources)
+
     return PokemonSchema(
         id=pokemon_id,
         name=pokemon.name,
